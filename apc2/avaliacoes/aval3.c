ad689b9d6d2e75d9b1ad53ca17b39e0e00a6a6f5
JPMendes27123 <joao.silva40@iesb.edu.br>
2025-05-15 17:25:28 -0300

    aval3 final amem

--- apc2/avaliacoes/aval3.c
+++ apc2/avaliacoes/aval3.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+    //funcao 1
 int *build_int_array(unsigned int size){
     int *v = malloc(sizeof (int) * size);
     return v;
 }
-
-float *media_das_notas(int m, int n, float matriz[m][n]){
+    //funcao 2
+float *medias_das_notas(int m, int n, float matriz[m][n]){
     float *media = (float*)malloc(sizeof (float) * m);
 
     for (int i = 0; i < m; i++) {
@@ -19,42 +20,45 @@ float *media_das_notas(int m, int n, float matriz[m][n]){
     }
     return media;
 }
-
+    //funcao 3
 char *repete_string(char str[], int n){
     int size_str = strlen(str);
-    int size1 = (size_str * n) + 1;
-    char *r = malloc(size1);
+    char *r = malloc(size_str * n + 1);
     
-    r[0] = '\0';
+    char *p = r;
     for (int i = 0; i < n; i++) {
-        strcat(r, str);
+        strcpy(p, str);
+        p += size_str;
     }
-
+    *p = '\0';
     return r;
 }
 
-int main() {
 
-int m = 3, n = 2;
-    float notas[3][2] = {
-        {5, 7},
-        {8, 7},
-        {6, 10}
-    };
-float *medias = medias_das_notas(m, n, notas);
+
+int main() {
+    //teste 1
+    int *v = build_int_array(10);
+    free(v);
+    //teste 2
+    float notas[3][2] = {{5,7}, {8,7}, {6,10}};
+    float *medias = medias_das_notas(3, 2, notas);
 
     if (medias != NULL) {
-        printf("Médias:\n");
-        for (int i = 0; i < m; i++) {
-            printf("Aluno %d: %.1f\n", i, medias[i]);
+        printf("Saida da funcao:\n");
+        printf("(float *) {");
+        for (int i = 0; i < 3; i++) {
+            printf("%.1f%s", medias[i], (i < 2) ? ", " : "");
         }
+        printf("}\n");
         free(medias);
     }
+    //teste 3
+    char *r = repete_string("Ana", 3);
 
-char *r = repete_string("Ana", 3);
-    
     if (r != NULL) {
-        printf("Resultado: %s\n", r);
+        printf("\nSaida da funcao:\n");
+        printf("(char *) \"%s\"\n", r);
         free(r);
     }
 
